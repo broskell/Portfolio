06d69c7f56d377aef5f5e81d9046bf29c45ecbbf
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-03-09 21:13:59 +0530

    Changed the nav bar icons to google fonts svg

--- shared.js
+++ shared.js
@@ -1,13 +1,13 @@
 // ── Shared navigation & keyboard shortcuts ──────────────────────────────────
 
 const pages = [
-  { key: '1', label: 'Home',       href: 'index.html',     icon: '⌂' },
-  { key: '2', label: 'About',      href: 'about.html',     icon: '◉' },
-  { key: '3', label: 'Education',  href: 'education.html', icon: '◈' },
-  { key: '4', label: 'Projects',   href: 'projects.html',  icon: '◆' },
-  { key: '5', label: 'Tech Stack', href: 'techstack.html', icon: '◇' },
-  { key: '6', label: 'Terminal',   href: 'terminal.html',  icon: '>_' },
-  { key: '7', label: 'Contact',    href: 'contact.html',   icon: '✉' },
+  { key: '1', label: 'Home',       href: 'index.html',     icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>' },
+  { key: '2', label: 'About',      href: 'about.html',     icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M440-280h80v-240h-80v240Zm68.5-331.5Q520-623 520-640t-11.5-28.5Q497-680 480-680t-28.5 11.5Q440-657 440-640t11.5 28.5Q463-600 480-600t28.5-11.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>' },
+  { key: '3', label: 'Education',  href: 'education.html', icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M270-80q-45 0-77.5-30.5T160-186v-558q0-38 23.5-68t61.5-38l395-78v640l-379 76q-9 2-15 9.5t-6 16.5q0 11 9 18.5t21 7.5h450v-640h80v720H270Zm90-233 200-39v-478l-200 39v478Zm-80 16v-478l-15 3q-11 2-18 9.5t-7 18.5v457q5-2 10.5-3.5T261-293l19-4Zm-40-472v482-482Z"/></svg>' },
+  { key: '4', label: 'Projects',   href: 'projects.html',  icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M160-240v-480 520-40Zm0 80q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v200h-80v-200H447l-80-80H160v480h200v80H160ZM584-56 440-200l144-144 56 57-87 87 87 87-56 57Zm192 0-56-57 87-87-87-87 56-57 144 144L776-56Z"/></svg>' },
+  { key: '5', label: 'Tech Stack', href: 'techstack.html', icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M480-400 40-640l440-240 440 240-440 240Zm0 160L63-467l84-46 333 182 333-182 84 46-417 227Zm0 160L63-307l84-46 333 182 333-182 84 46L480-80Zm0-411 273-149-273-149-273 149 273 149Zm0-149Z"/></svg>' },
+  { key: '6', label: 'Terminal',   href: 'terminal.html',  icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M480-160v-80h320v80H480ZM220-320l-56-56 183-184-183-184 56-56 240 240-240 240Z"/></svg>' },
+  { key: '7', label: 'Contact',    href: 'contact.html',   icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>' },
 ];
 
 function buildNav() {
